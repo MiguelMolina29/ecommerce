3a125742c16c0d77dc4f49893a4c19e3cbc2313c
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2022-05-24 16:50:53 -0500

    docs

---
+++ docs/precache-manifest.55ba108bb2869ec13c2e9d10cbb374e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26a0838720b590e8e72a17e4d7fa5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d59b12834f14882efdc7",
+    "url": "/static/css/2.055bdf39.chunk.css"
+  },
+  {
+    "revision": "39d4b1577c45bebc206c",
+    "url": "/static/css/main.2b0bb07c.chunk.css"
+  },
+  {
+    "revision": "d59b12834f14882efdc7",
+    "url": "/static/js/2.ed6118f4.chunk.js"
+  },
+  {
+    "revision": "7d14e27a7aaa03b06d37f1a110048409",
+    "url": "/static/js/2.ed6118f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d4b1577c45bebc206c",
+    "url": "/static/js/main.84229004.chunk.js"
+  },
+  {
+    "revision": "1e96778912d0fe1a4cb3",
+    "url": "/static/js/runtime-main.69f752e8.js"
+  },
+  {
+    "revision": "e56c3501b5bed62db90ad3387d226b00",
+    "url": "/static/media/logo.e56c3501.png"
+  },
+  {
+    "revision": "64821c92123087604b85694a45c442e3",
+    "url": "/static/media/slider01.64821c92.webp"
+  },
+  {
+    "revision": "8e56719504e87ac056738550138ca0b2",
+    "url": "/static/media/slider02.8e567195.webp"
+  },
+  {
+    "revision": "7f65227f287fa2394c91d92fd13dc3c6",
+    "url": "/static/media/slider03.7f65227f.webp"
+  }
+]);
